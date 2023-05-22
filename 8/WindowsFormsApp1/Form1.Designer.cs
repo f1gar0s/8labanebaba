6a084fa38a4ae0f032aa1beb5e47df793c554123
f1gar0s <125069408+f1gar0s@users.noreply.github.com>
2023-05-22 15:53:39 +0300

    Add files via upload

---
+++ 8/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstМеню = new System.Windows.Forms.ListBox();
+            this.lstЗаказ = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.mnuContext = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuContext.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(40, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Меню:";
+            // 
+            // lstМеню
+            // 
+            this.lstМеню.ContextMenuStrip = this.mnuContext;
+            this.lstМеню.FormattingEnabled = true;
+            this.lstМеню.Items.AddRange(new object[] {
+            "Глазунья",
+            "Гусь жаренный",
+            "Зелёный горошек",
+            "Кильки в том. соусе",
+            "Компот",
+            "Мундиры",
+            "Сахар для чая",
+            "Сухари",
+            "Цветная капуста"});
+            this.lstМеню.Location = new System.Drawing.Point(44, 101);
+            this.lstМеню.Name = "lstМеню";
+            this.lstМеню.Size = new System.Drawing.Size(199, 277);
+            this.lstМеню.TabIndex = 1;
+            this.lstМеню.SelectedIndexChanged += new System.EventHandler(this.lstМеню_SelectedIndexChanged);
+            // 
+            // lstЗаказ
+            // 
+            this.lstЗаказ.FormattingEnabled = true;
+            this.lstЗаказ.Location = new System.Drawing.Point(551, 101);
+            this.lstЗаказ.Name = "lstЗаказ";
+            this.lstЗаказ.Size = new System.Drawing.Size(199, 277);
+            this.lstЗаказ.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label2.Location = new System.Drawing.Point(547, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Заказ:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(340, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 44);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Добавить ->";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(340, 213);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 40);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "<- Убрать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(340, 292);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 39);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Заказать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // mnuContext
+            // 
+            this.mnuContext.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4});
+            this.mnuContext.Name = "mnuContext";
+            this.mnuContext.Size = new System.Drawing.Size(221, 114);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(220, 22);
+            this.toolStripMenuItem1.Text = "Добавить новое блюдо";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Enabled = false;
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(220, 22);
+            this.toolStripMenuItem2.Text = "Заказать блюдо";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(220, 22);
+            this.toolStripMenuItem3.Text = "Удалить блюдо";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(220, 22);
+            this.toolStripMenuItem4.Text = "Изменить название блюда";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstЗаказ);
+            this.Controls.Add(this.lstМеню);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.mnuContext.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstМеню;
+        private System.Windows.Forms.ListBox lstЗаказ;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ContextMenuStrip mnuContext;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+    }
+}
+
---
+++ 8/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            if (lstМеню.SelectedIndex >= 0)
+            {
+                lstЗаказ.Items.Add(lstМеню.SelectedItem);
+            }
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            
+            if (lstЗаказ.SelectedItem != null)
+            {
+                lstЗаказ.Items.Remove(lstЗаказ.SelectedItem);
+            }
+        }
+
+        public void button3_Click(object sender, EventArgs e)
+        {
+            if (lstЗаказ.Items.Count>0)
+            {
+                MessageBox.Show("Приятного аппетита!", "Ресторан «Студент»",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Ваш заказ пуст!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstМеню_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            toolStripMenuItem2.Enabled = true;
+            toolStripMenuItem3.Enabled = true;
+            toolStripMenuItem4.Enabled = true;
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            string s = Microsoft.VisualBasic.Interaction.InputBox("Введите название нового блюда","Ресторан «Студент»", " < Новое блюдо > ");
+            if (s == "") return;
+            lstМеню.Items.Add(s);
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            string i = Microsoft.VisualBasic.Interaction.InputBox("Введите название блюда", "Ресторан «Студент»", " < Удалить блюдо > ");
+            if (i == "") return;
+            lstМеню.Items.Remove(i);
+        }
+
+        public void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            if (lstМеню.SelectedItem != null)
+            {
+                string a = Microsoft.VisualBasic.Interaction.InputBox("Введите новое название блюда", "Ресторан «Студент»", lstМеню.SelectedItem.ToString());
+                if (a != "")
+                {
+                    lstМеню.Items.[lstМеню.SelectedIndex]=a;
+                }
+            }
+        }
+    }
+}
